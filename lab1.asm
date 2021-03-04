940e927969e5324814b1b9bd3949d715ee6463d9
emadhamad171 <48877111+emadhamad171@users.noreply.github.com>
2021-03-04 08:43:44 +0200

    Add files via upload

---
+++ lab1.asm
@@ -0,0 +1,27 @@
+IDEAL
+MODEL small
+stack 256
+
+DATASEG
+exCode db 0
+message1 db "Team 6: Anton Chaban",10, 13, "Hamad Emad",10 ,13, "Nickita Filianin",10,13, "Emad",10,13, '$'
+
+CODESEG
+Start:
+mov ax,@data ; mov приемник, источник
+mov ds, ax ; записуємо в сегмент данних данні з регістру ax
+mov es, ax ; записуємо в додатковий сегмент данні з регістру ax
+mov dx, offset message1 ; передаємо адресу пам'яті на наш меседж, 
+						; так як ми не використовували символ '$', передаємо лише 1 меседж
+mov ah, 09h ; AH = 09h - WRITE STRING TO STANDARD OUTPUT
+int 21h ; функція переривання 21h
+; mov ah, 9
+int 21h
+mov ah,01h ; AH = 01h - READ CHARACTER FROM STANDARD INPUT, WITH ECHO
+int 21h
+mov ah,4ch ; AH = 4Ch - "EXIT" - TERMINATE WITH RETURN CODE
+mov al,[exCode]; записуємо значення exCode в молодший байт регістру ax
+int 21h
+end Start 
+
+
